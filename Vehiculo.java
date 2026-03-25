1290a6db131b33b2fab76a8f42e55a83bb01bd43
Angel-Emmanuel-6174 <20253rd023@utez.edu.mx>
2026-03-25 16:59:31 -0600

    Se termino la clase Vehiculo, con lod getters, setters, construcores y metodos

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,5 +1,55 @@
 public class Vehiculo {
     private String marca, modelo, anio;
-    private double precio; 
+    private double precio;
+    static int totalVehiculos = 0;
+    public Vehiculo(){
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getAnio() {
+        return anio;
+    }
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Vehiculo(String marca, String modelo, String anio, double precio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+        this.precio = precio;
+    }
+
+    public void mostrarInfo(){
+        System.out.println("Marca: " + this.marca);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Anio: " + this.anio);
+    }
+
+    public void encender(){
+        System.out.println("El vehiculo "+this.marca+" ha sido encendido");
+    }
+
+    public void aplicarDescuento(){
+        double desc = 0.9;
+        System.out.println("TOTAL CON DESCUENTO: "+(this.precio*desc));
+    }
 
 }
